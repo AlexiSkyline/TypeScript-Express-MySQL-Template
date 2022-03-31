3b4576bc74f4e9281b188085190c42398f5538d6
AlexiSkyline <ilegal_sprite@outlook.com>
2022-03-30 23:05:14 -0600

    Agregamos la funcionalidad de crear Usuario

--- controller/Usuarios.ts
+++ controller/Usuarios.ts
@@ -18,13 +18,25 @@ export const getUsuario = async ( req: Request, res: Response ) => {
     res.json( usuario );
 }
 
-export const postUsuario = ( req: Request, res: Response ) => {
+export const postUsuario = async( req: Request, res: Response ) => {
     const { body } = req;
 
-    res.json({
-        msg: 'PostUsuario',
-        body
-    });
+    try {
+        const existeEmail = await Usuario.findOne({ where: { email: body.email } });
+        if( existeEmail ) {
+            return res.status(400).json({ msg: 'El email ya existe' });
+        }
+        
+        const usuario = new Usuario( body );
+        await usuario.save();
+
+        res.json( usuario );
+    } catch (error) {
+        console.log( error );
+        res.status(500).json({
+            msg: 'Hable con el administrador',
+        });
+    }
 }
 
 export const putUsuario = ( req: Request, res: Response ) => {
