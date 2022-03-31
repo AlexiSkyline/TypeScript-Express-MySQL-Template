a0eb4734192e14caf95c4ac9d2908390474db17f
AlexiSkyline <ilegal_sprite@outlook.com>
2022-03-30 22:44:09 -0600

    Agregamos la funcionalidad de getUsuarios

--- controller/Usuarios.ts
+++ controller/Usuarios.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from 'express';
+import Usuario from '../models/Usuario';
 
-export const getUsuarios = ( req: Request, res: Response ) => {
-    res.json({
-        msg: 'getUsuarios'
-    });
+export const getUsuarios = async ( req: Request, res: Response ) => {
+    const usuarios = await Usuario.findAll();
+    
+    res.json({ usuarios });
 }
 
 export const getUsuario = ( req: Request, res: Response ) => {
